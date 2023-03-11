899d1ba93d96e9a570cc97c07af87b20d389202a
Gastti <gutigas96@gmail.com>
2023-03-11 00:54:41 -0300

    feat: add paragraph component

---
+++ src/components/Paragraph/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Paragraph = styled.p`
+    color: rgb(167, 167, 167);
+    font-family: var(--f-ubuntu);
+    font-size: 1.3rem;
+    font-weight: 300;
+`;
