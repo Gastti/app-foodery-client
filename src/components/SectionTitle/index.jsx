5ba374ac7d2726b3e016049de0ffa6abb46dbc9e
Gastti <gutigas96@gmail.com>
2023-03-11 00:54:59 -0300

    feat: add sectiontitle component

---
+++ src/components/SectionTitle/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+export const SectionTitle = styled.div`
+    color: var(--c-primary);
+    font-family: var(--f-ubuntu);
+    font-size: 1.5rem;
+`;
