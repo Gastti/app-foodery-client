a8db62a362bdc9e691f515f5e1021e474c271e28
Gastti <gutigas96@gmail.com>
2023-03-20 21:54:46 -0300

    feat: add divider component

---
+++ src/components/Divider/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Divider = styled.div`
+    width: 100%;
+    height: 1px;
+    background-color: #dbdbdb;
+    margin: 15px 0px;
+`
+
+export default Divider;
