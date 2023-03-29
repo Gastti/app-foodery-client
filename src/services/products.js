1ed7ba7107a46fa938ffdf4b37a11b606c35576c
Gastti <gutigas96@gmail.com>
2023-03-28 22:10:00 -0300

    feat: add loadPopularFood request function

--- src/services/products.js
+++ src/services/products.js
@@ -8,16 +8,34 @@ const loadProducts = async (cb, limit = 25, page = 1) => {
 
 const loadProductsCategory = async (category, limit, page) => {
     const response = await fetch(`${API_URL}/products/search?category=${category}&limit=${limit}&page=${page}`)
+
     if (!response.ok) {
         console.log('Error en loadProductsQuery');
     }
 
     const { data } = await response.json();
-    console.log(data);
     return data;
+}
+
+const loadPopularProducts = async () => {
+    try {
+        const response = await fetch(`${API_URL}/products/popular`);
+
+        if (!response.ok) {
+            console.log('Error en loadPopularProducts');
+            return [];
+        }
+
+        const { data } = await response.json();
+        return data;
+    } catch (error) {
+        console.log('Error en loadPopularProducts');
+        return [];
+    }
 }
 
 export {
     loadProducts,
-    loadProductsCategory
+    loadProductsCategory,
+    loadPopularProducts
 }
