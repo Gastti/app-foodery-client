bf2e1403f23d93c995fa9f9707f5ba2e3081706c
Gastti <gutigas96@gmail.com>
2023-03-29 17:55:22 -0300

    feat: add product, catalog and searcher routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navbar from './components/Navbar';
-import BrowsePage from './pages/BrowsePage/BrowsePage';
+import Catalog from './pages/Catalog';
 import OrderPage from './pages/OrderPage/OrderPage';
 import DeliveryPage from './pages/DeliveryPage/DeliveryPage';
 import AuthPage from './pages/AuthPage/AuthPage';
 import WelcomePage from './pages/WelcomePage/WelcomePage';
+import Searcher from './pages/Searcher';
 import { AuthProvider, AuthRoute } from './services/auth';
 import { ConfigProvider } from './contexts/ConfigContext';
+import Footer from './components/Footer';
+import Product from './pages/Product';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
@@ -16,12 +20,15 @@ function App() {
       <ConfigProvider>
         <AuthProvider>
           <Navbar />
+          <ShoppingCart />
           <Routes>
             <Route path="/" element={
               <Home />
             } />
 
-            <Route path="/browse" element={<BrowsePage />} />
+            <Route path="/catalog" element={<Catalog />} />
+            <Route path="/search/:slug" element={<Searcher />} />
+            <Route path="/product/:product_id" element={<Product />} />
             <Route path="/order" element={<OrderPage />} />
             <Route path="/delivery" element={<DeliveryPage />} />
             <Route path="/auth" element={<AuthPage />} />
@@ -36,6 +43,7 @@ function App() {
               </AuthRoute>
             } />
           </Routes>
+          <Footer />
         </AuthProvider>
       </ConfigProvider>
     </HashRouter>
