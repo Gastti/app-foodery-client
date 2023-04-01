00ad63f33cd3db59ae37922c83be50c288733d9c
Gastti <gutigas96@gmail.com>
2023-03-31 23:18:57 -0300

    feat: add new fetchUser function and remove old one

--- src/services/user.js
+++ src/services/user.js
@@ -1,30 +1,4 @@
 import { API_URL } from './config';
-import { useToken } from '../hooks/useToken';
-
-const loadUser = async (token, cb) => {
-    const { removeToken } = useToken();
-
-    if (token) {
-        fetch(`${API_URL}/users/me`, {
-            method: 'GET',
-            headers: { 'Authorization': `Bearer ${token}` }
-        })
-            .then(response => {
-                if (response.ok) return response.json();
-                else throw new Error(`HTTP error! status: ${response.status}`);
-
-            })
-            .then(data => {
-                cb(data.data)
-                console.log(data.data);
-            })
-            .catch(error => {
-                if (error.message.includes('401')) {
-                    removeToken();
-                }
-            })
-    }
-}
 
 const fetchUser = async (token) => {
     try {
